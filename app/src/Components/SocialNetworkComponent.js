2af51137630644125f8c456ea756f626cc9d3594
Aloy Das <gate.2017.aloy@gmail.com>
2022-02-14 21:40:53 +0530

    Modify Social Media Buttons

--- app/src/Components/SocialNetworkComponent.js
+++ app/src/Components/SocialNetworkComponent.js
@@ -2,8 +2,9 @@ import React from "react";
 import {TripleTextComponent} from "./Utils/TripleTextComponent";
 import {Space} from "antd";
 import {Link} from "@mui/material";
-import {faDiscord, faTwitter, faTelegram} from "@fortawesome/free-brands-svg-icons"
+import {faDiscord, faTwitter, faTelegram, faMedium} from "@fortawesome/free-brands-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {makeStyles} from "@mui/styles";
 
 const propsForTripleTextComponent = {
     header: "Connect with us",
@@ -11,21 +12,39 @@ const propsForTripleTextComponent = {
     bottomText: <>Jump right into the Xtake Project.</>
 }
 
+const hexCodes = {
+    Twitter: "#1DA1F2",
+    Discord: "#5663F7",
+    Telegram: "#229ED9",
+    Medium: "black"
+}
+
+
 export const SocialMediaComponent = ({icon, text, href}) => {
+
+    const useStyles = makeStyles({
+        item: {
+            borderRadius: "10px",
+            width: 200,
+            background: "#1A1A1A",
+            textAlign: "center",
+            padding: "10%",
+            border: "1px solid #333333",
+            color: "white",
+            '&:hover': {
+                background: hexCodes[text],
+            },
+        }
+    })
+    const classes = useStyles();
+
+
     return (
         <Link target="_blank" href={href}
               underline={"none"}
         >
             <div
-                style={{
-                    borderRadius: "10px",
-                    width: 200,
-                    background: "#1A1A1A",
-                    textAlign: "center",
-                    padding: "10%",
-                    border: "1px solid #333333",
-                    color: "white"
-                }}
+                className={classes.item}
             >
                 <FontAwesomeIcon size={"lg"} color={"white"} icon={icon}/> &nbsp; {text}
             </div>
@@ -43,6 +62,7 @@ export const SocialNetworkComponent = () => {
                 <SocialMediaComponent icon={faTwitter} text={"Twitter"} href={"https://twitter.com/XtakeFinance"}/>
                 <SocialMediaComponent icon={faDiscord} text={"Discord"} href={"https://discord.gg/fn3RPVM3Kq"}/>
                 <SocialMediaComponent icon={faTelegram} text={"Telegram"} href={"https://t.me/XtakeOfficial"}/>
+                <SocialMediaComponent icon={faMedium} text={"Medium"} href={"https://xtake.medium.com/"}/>
             </Space>
         </div>
     )
